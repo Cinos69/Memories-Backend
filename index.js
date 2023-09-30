7944dc666af5fc7534da217f5971613bb6de8bb9
Cinos Codes <mankame96@gmail.com>
2023-09-30 23:01:21 +0530

    update-3

--- index.js
+++ index.js
@@ -17,6 +17,10 @@
 
  app.use("/posts", postRoutes);
 
+ app.get("/",(req,res) => {
+    res.send("Hello memories API");
+ });
+
  const CONNECTION_URL = process.env.CONNECTION_URL;
  const PORT = process.env.PORT || 5000;
 
